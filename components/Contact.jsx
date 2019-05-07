192563ac1b6c4e359cfa96d16d7211034d04d74d
KnightRider <omar.brown@comcast.net>
2019-05-07 08:55:14 -0400

    Second Commit

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,1 +1,18 @@
 import React from 'react';
+
+
+ class Contact React.Component {
+ render() {
+  return (
+    <div id="contact">
+    <h1> Store Hours</h1>
+    <p>Sunday-Thursday</p>
+    <div>
+        <p>phone;</p>
+        <p>email</p>
+    </div>
+    </div>
+  );
+  }
+ }
+ export default Contact;
